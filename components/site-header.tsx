86c55d55be103e20d3ff9a394f30888225038098
Alison Tahiri <alison.tahiri@gmail.com>
2023-08-21 15:10:18 +0200

    add search functionality

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React from "react"
 import Link from "next/link"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Edit, ShoppingBag } from "lucide-react"
@@ -12,13 +13,27 @@ import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   const pathName = usePathname()
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const defaultSearchQuery = searchParams.get("search") ?? ""
+
   if (pathName.startsWith("/studio")) return null
+
+  function onSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const searchQuery = formData.get("search")
+    router.replace(searchQuery ? `/?search=${searchQuery}` : "")
+  }
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
         <MainNav />
-        <form className="hidden items-center lg:inline-flex">
+        <form
+          onSubmit={onSubmit}
+          className="hidden items-center lg:inline-flex"
+        >
           <Input
             id="search"
             name="search"
@@ -26,6 +41,7 @@ export function SiteHeader() {
             autoComplete="off"
             placeholder="Search products..."
             className="h-9 lg:w-[300px]"
+            defaultValue={defaultSearchQuery}
           />
         </form>
         <div className="flex items-center space-x-1">
